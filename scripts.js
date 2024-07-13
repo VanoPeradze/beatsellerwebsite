3c0443c0ee29c81925ee685cee904eea3ccc841b
unknown <vano17p@gmail.com>
2024-07-13 15:22:46 +0300

    fourth commit

--- scripts.js
+++ scripts.js
@@ -5,3 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Website loaded');
     // Add any additional JavaScript here
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const audioElements = document.querySelectorAll('audio');
+
+    audioElements.forEach(audio => {
+        audio.addEventListener('play', () => {
+            audioElements.forEach(otherAudio => {
+                if (otherAudio !== audio) {
+                    otherAudio.pause();
+                    otherAudio.currentTime = 0;
+                }
+            });
+        });
+    });
+});
